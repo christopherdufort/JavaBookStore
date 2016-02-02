e383e62c41db205b431b0e50d9abc9b2c02a53b5
Jonas Faure <jonas.faure.etu@gmail.com>
2016-02-02 13:20:58 -0500

    adding GenreBean

---
+++ CSDBookStore/src/main/java/com/g3w16/beans/GenreBean.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g3w16.beans;
+
+/**
+ *
+ * @author jesuisnuageux
+ */
+public class GenreBean {
+    private String name;
+
+    public GenreBean() {
+    }
+
+    public GenreBean(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+}
