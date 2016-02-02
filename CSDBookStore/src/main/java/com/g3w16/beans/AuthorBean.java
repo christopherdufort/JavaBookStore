7e424709ad8a15352d263fb8e15d2cdf7f1c6dad
Jonas Faure <jonas.faure.etu@gmail.com>
2016-02-02 13:18:34 -0500

    adding AuthorBean

---
+++ CSDBookStore/src/main/java/com/g3w16/beans/AuthorBean.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g3w16.beans;
+
+/**
+ *
+ * @author jesuisnuageux
+ */
+public class AuthorBean {
+    private String name;
+
+    public AuthorBean(){}
+    
+    public AuthorBean(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+       
+}
