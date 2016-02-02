0e0d5180482ce3878c9cb991d5e11a237636141a
Jonas Faure <jonas.faure.etu@gmail.com>
2016-02-02 13:22:29 -0500

    adding FormatBean

---
+++ CSDBookStore/src/main/java/com/g3w16/beans/FormatBean.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g3w16.beans;
+
+/**
+ *
+ * @author jesuisnuageux
+ */
+public class FormatBean {
+    private String extension;
+
+    public FormatBean() {
+    }
+
+    public FormatBean(final String extension) {
+        this.extension = extension;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(final String extension) {
+        this.extension = extension;
+    }
+    
+    
+}
