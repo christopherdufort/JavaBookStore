180b43578a69abdd78bbc0cebf33213d94a0cb6f
Xin Ma <maxin911.ca@gmail.com>
2016-03-09 13:33:24 -0500

    Add support for manager authentication

--- CSDBookStore/src/main/java/com/g3w16/actionController/AuthenticationController.java
+++ CSDBookStore/src/main/java/com/g3w16/actionController/AuthenticationController.java
@@ -38,6 +38,22 @@ public class AuthenticationController implements SystemEventListener, Serializab
         Logger.getLogger(AuthenticationController.class.getName()).log(Level.INFO, "AuthenticatedUser value is = {0}", (authenticatedUser.getRegisteredUser()==null));
         if ( authenticatedUser.getRegisteredUser()==null){
             FacesContext.getCurrentInstance().getExternalContext().redirect("faces/login.xhtml");
+        }else{
+            if (!userController.isClient(authenticatedUser.getRegisteredUser())){
+                FacesContext.getCurrentInstance().getExternalContext().redirect("faces/login.xhtml");
+            }
+        }
+    }
+    
+    public void mustBeManager(ComponentSystemEvent event) throws IOException{
+        Logger.getLogger(AuthenticationController.class.getName()).log(Level.INFO, "AuthenticationController.isClient invoked !");
+        Logger.getLogger(AuthenticationController.class.getName()).log(Level.INFO, "AuthenticatedUser value is = {0}", (authenticatedUser.getRegisteredUser()==null));
+        if ( authenticatedUser.getRegisteredUser()==null){
+            FacesContext.getCurrentInstance().getExternalContext().redirect("faces/login.xhtml");
+        }else{
+            if (!userController.isManager(authenticatedUser.getRegisteredUser())){
+                FacesContext.getCurrentInstance().getExternalContext().redirect("faces/login.xhtml");
+            }
         }
     }
     
@@ -58,6 +74,14 @@ public class AuthenticationController implements SystemEventListener, Serializab
         }
         return userController.isClient(authenticatedUser.getRegisteredUser());
     }
+    
+    public boolean isManager(){
+        if (authenticatedUser.getRegisteredUser() == null){
+            return false;
+        }
+        return userController.isManager(authenticatedUser.getRegisteredUser());
+    }
+
 
     @Override
     public void processEvent(SystemEvent se) throws AbortProcessingException {
