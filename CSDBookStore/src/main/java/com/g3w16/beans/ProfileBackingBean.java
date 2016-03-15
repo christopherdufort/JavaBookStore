ae6c7084d40ff5f6e231cf0196ae71ed2d8a06d5
Christopher Dufort <christopher.dufort@outlook.com>
2016-03-15 14:44:17 -0400

    Version 0.4.2-SNAPSHOT
    ProfileBackingBean can pull user details but not persist them.

--- CSDBookStore/src/main/java/com/g3w16/beans/ProfileBackingBean.java
+++ CSDBookStore/src/main/java/com/g3w16/beans/ProfileBackingBean.java
@@ -11,9 +11,12 @@ import com.g3w16.entities.RegisteredUserJpaController;
 import com.g3w16.entities.RegisteredUser;
 import com.g3w16.entities.Title;
 import com.g3w16.entities.TitleJpaController;
+import com.g3w16.entities.exceptions.RollbackFailureException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -28,18 +31,16 @@ import javax.inject.Named;
 public class ProfileBackingBean implements Serializable {
     
     @Inject
+    private AuthenticatedUser authenticatedUser;
+    @Inject
     private RegisteredUserJpaController registeredUserJpaController;
     @Inject
     private TitleJpaController titleJpaController;
     @Inject
     private ProvinceJpaController provinceJpaController;
     
-    private RegisteredUser registeredUser;
-    
-    private String selectedTitle;
     private List<Title> availableTitles;
     
-    private String selectedProvince;
     private List<Province> availableProvinces;
     
     public ProfileBackingBean(){
@@ -51,25 +52,30 @@ public class ProfileBackingBean implements Serializable {
         availableTitles = titleJpaController.findAll();
         availableProvinces = provinceJpaController.findAll();
     }
-    public RegisteredUser getRegisteredUser(){
-        if (registeredUser == null){
-            registeredUser = new RegisteredUser();
-        }
-        return registeredUser;
-    }
-    public String getSelectedTitle(){
-        return selectedTitle;
+    
+    public AuthenticatedUser getAuthenticatedUser(){
+        return authenticatedUser;
     }
     
     public List<Title> getAvailableTitles(){
         return availableTitles;
     }
-    
-    public String getSelectedProvince(){
-        return selectedProvince;
-    }
-    
+      
     public List<Province> getAvailableProvinces(){
         return availableProvinces;
+    }
+    
+    public String persistChangesToProfile(AuthenticatedUser authenticatedUser){
+        try {
+            registeredUserJpaController.edit(authenticatedUser.getRegisteredUser());
+        } catch (RollbackFailureException ex) {
+            Logger.getLogger(ProfileBackingBean.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("ERROR ROLL BACK IN ProfileBackinggBean");
+        } catch (Exception ex) {
+            Logger.getLogger(ProfileBackingBean.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("ERROR EXCEPTION IN ProfileBackingBean");
+        }
+        System.out.println("Changes made to profile -> returning home");
+        return "home"; // TODO: Change that, it's ugly to use hardcoded filename to redirect !!
     }
 }
