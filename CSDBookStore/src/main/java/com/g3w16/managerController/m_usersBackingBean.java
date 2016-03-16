813e03c56042081426a95a92f2f54c55db89de72
Xin Ma <maxin911.ca@gmail.com>
2016-03-16 16:25:40 -0400

    add m_user methods

--- CSDBookStore/src/main/java/com/g3w16/managerController/m_usersBackingBean.java
+++ CSDBookStore/src/main/java/com/g3w16/managerController/m_usersBackingBean.java
@@ -9,11 +9,15 @@ import com.g3w16.entities.*;
 import com.g3w16.entities.exceptions.RollbackFailureException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.model.SelectItem;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -22,18 +26,64 @@ import javax.inject.Named;
  * @author Xin Ma
  * @author Rita Lazaar
  */
+
 @Named("m_users")
 @RequestScoped
 public class m_usersBackingBean {
 
     @Inject
-    RegisteredUser registeredUser;
+    RegisteredUser user;
     
     @Inject
     RegisteredUserJpaController userJpa;
+    
+    @Inject
+    Title title;
+    
+    @Inject 
+    TitleJpaController titleJpa;
+    
+    @Inject
+    Province province;
+    
+    @Inject
+    ProvinceJpaController provinceJpa;
+    
+    public RegisteredUser getSelectedUser() {
+        if (user == null) {
+            user = new RegisteredUser();
+        }
+        return user;
+    }
+
+    public String editUser(RegisteredUser u) {
+        user = userJpa.findUserById(u.getUserId());
+        return "m_editUser";
+    }
+
+    public String updateUser() {
+        try {
+            userJpa.edit(user);
+        } catch (Exception ex) {
+            Logger.getLogger(m_usersBackingBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "m_users";
+    }
 
     public List<RegisteredUser> getAllUsers() {
         return userJpa.findAll();
 
+    }
+
+    public int getUserCount() {
+        return userJpa.getUsersCount();
+    }
+
+    public List<Title> getAllTitle() {
+        return titleJpa.findAll();
+    }
+    
+    public List<Province> getAllProvince(){
+        return provinceJpa.findAll();
     }
 }
