bdce1b5b01f13adaf266658698f39b81e9fd62c4
rl <rita.lazaar@dawsoncollege.qc.ca>
2016-03-29 20:53:39 -0400

    Added converters - books

---
+++ CSDBookStore/src/main/java/com/g3w16/converter/AuthorConverter.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g3w16.converter;
+
+import com.g3w16.entities.AuthorJpaController;
+import com.g3w16.entities.BookJpaController;
+import javax.enterprise.inject.spi.CDI;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author Rita Lazaar
+ */
+@FacesConverter("com.g3w16.converter.AuthorConverter")
+public class AuthorConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        AuthorJpaController authorJpa = CDI.current().select(AuthorJpaController.class).get();
+        return authorJpa.findAuthor(Integer.parseInt(value));
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        return value.toString();
+    }
+
+}
---
+++ CSDBookStore/src/main/java/com/g3w16/converter/FormatConverter.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g3w16.converter;
+
+import com.g3w16.entities.AuthorJpaController;
+import com.g3w16.entities.BookJpaController;
+import com.g3w16.entities.FormatJpaController;
+import javax.enterprise.inject.spi.CDI;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author Rita Lazaar
+ */
+@FacesConverter("com.g3w16.converter.FormatConverter")
+public class FormatConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        FormatJpaController formatJpa = CDI.current().select(FormatJpaController.class).get();
+        return formatJpa.findFormat(Integer.parseInt(value));
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        return value.toString();
+    }
+
+}
