10ba8a22eaa979ea687bb4ef0a8b0c3375270716
rl <rita.lazaar@dawsoncollege.qc.ca>
2016-03-31 20:51:23 -0400

    added stock reports - modifications in sales

--- CSDBookStore/src/main/java/com/g3w16/managerController/m_invoicesBackingBean.java
+++ CSDBookStore/src/main/java/com/g3w16/managerController/m_invoicesBackingBean.java
@@ -97,14 +97,6 @@ public class m_invoicesBackingBean implements Serializable{
      */
     public List<Invoice> getAllInvoices() {
 
-
-//        all = invoiceJpa.findInvoiceEntities();
-//        //  all= invoiceJpa.findInvoiceEntities();
-////         for(int i =0; i<invoiceJpa.findInvoiceEntities().size();i++){
-////         
-////             all.add(invoice)
-////         }
-
         return all;
     }
 
