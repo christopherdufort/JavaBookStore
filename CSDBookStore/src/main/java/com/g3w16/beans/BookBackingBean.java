4c6000b65dbb1cb2b8687b8248b285d7cbc9e90e
Joey <Joey@DesktopPC>
2016-04-05 15:16:15 -0400

    fixed some tag issues, fixed growl problem in book page

--- CSDBookStore/src/main/java/com/g3w16/beans/BookBackingBean.java
+++ CSDBookStore/src/main/java/com/g3w16/beans/BookBackingBean.java
@@ -299,7 +299,7 @@ public class BookBackingBean implements Serializable {
         );
         cookie.setMaxAge(forever);
         ((HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse()).addCookie(cookie);
-        return "book?faces-redirect=true";
+        return "book";
     }
 
 }
